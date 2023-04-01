e7ffe11681f14976e2949914b74193cab4ac9b19
Mikael Kilpeläinen <mikael@securepath.fi>
2023-04-01 22:10:00 +0300

    add basic stucture and fix cmake

---
+++ test/tests.cpp
@@ -0,0 +1,28 @@
+
+
+//#define CATCH_CONFIG_RUNNER
+//#include <external/catch/catch.hpp>
+
+int main(int argc, char* argv[]) {
+/*	using namespace Catch::clara;
+
+    Catch::Session session;
+    bool show_logging = false;
+
+    auto cli = session.cli()
+        | Opt(show_logging)
+             ["--show-ssh-log"]
+             ("Show ssh log");
+
+    session.cli(cli);
+
+    auto ret = session.applyCommandLine( argc, argv );
+    if (ret) {
+        return ret;
+    }
+    if(show_logging) {
+    	ssh::test::test_log().set_level(stdout_logger::log_all);
+    }
+
+    return session.run();*/
+}
