4ce4fb3bb1512296a85bf935c6e1c8af4c15b90a
Mikael Kilpeläinen <mikael@securepath.fi>
2023-08-05 21:36:20 +0300

    add settings

--- spleak/settings.hpp
+++ spleak/settings.hpp
@@ -3,8 +3,17 @@
 namespace securepath::spleak {
 
 struct settings {
+	// Should we save stack trace, this is needed if wanting to see stack trace
+	// for leaked memory.
 	bool save_trace_to_allocation{true};
+	// Try to resolve module handle for allocations/frees and see if those match.
 	bool resolve_module{true};
+	// Collect general statistics for memory allocations.
+	bool collect_memory_allocation_statistics{false};
+	// Try to find memory allocation patterns.
+	bool analyse_allocation_patterns{false};
+	// Try to find memory owner cycles
+	bool analyse_memory_owner_cycles{false};
 };
 
 }
